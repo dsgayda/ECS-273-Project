5a02dc34df8c3f6530ff56f974345b9668734642
Namrata Kasaraneni <nammy.kasaraneni@gmail.com>
2023-03-04 16:53:25 -0800

    wrote backend for scatterplot visualization

--- Vue-Flask-Template/server/app.py
+++ Vue-Flask-Template/server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
-from controller import processExample
+from controller import processPolicyScatterPlot
 
 app = Flask(__name__)
 CORS(app)
@@ -10,19 +10,13 @@ CORS(app)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/fetchExample", methods=["GET", "POST"])
+
+@app.route("/fetchPolicyScatterplot", methods=["GET"])
 @cross_origin()
 def fetchExample():
-    if request.method == "GET": # handling GET request
-        points, cluster_names = processExample()
-        resp = jsonify(data=points, clusters=cluster_names)
-        return resp
-    else: # handling POST request, which is only effective when ExampleWithInteractions.vue is loaded
-        request_context = request.get_json() # JSON object
-        method = request_context['method']
-        points, cluster_names = processExample(method)
-        resp = jsonify(data=points, clusters=cluster_names)
-        return resp
+    points = processPolicyScatterPlot()
+    resp = jsonify(data=points)
+    return resp
 
 
 if __name__ == "__main__":
--- Vue-Flask-Template/server/controller.py
+++ Vue-Flask-Template/server/controller.py
@@ -1,25 +1,39 @@
 import pandas as pd
 import numpy as np
-from sklearn.datasets import load_wine
-from resources.hd_processing_template import perform_PCA, perform_TSNE
-#from resources.network_process_template import contsruct_networkx
-#from resources.text_processing_template import preprocess
-#from resources.time_processing_template import prepare_time_template_data, apply_arima, apply_sarima
 
-def processExample(method: str = 'PCA') -> tuple[list[dict], list[int]]:
-    data: dict = load_wine()
-    X: np.ndarray = data.data
-    y: np.ndarray = data.target
-    #feat_names: np.ndarray = data.feature_names
-    target_names: np.ndarray = data.target_names
+from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+
+
+def processPolicyScatterPlot(num_clusters: int = 3, method: str = 'PCA'):
+    # load data
+    data_filepath = "../server/data/policyDatabase.xlsx"
+    df = pd.read_excel(data_filepath)
+    X = df.drop(columns=['state', 'year', 'lawtotal'])
+
+    # cluster data
+    clusterer = KMeans(n_clusters=num_clusters, init='k-means++')
+    predictions = clusterer.fit_predict(X)
 
     if method == 'PCA':
-        Z, _ = perform_PCA(X)
+        pca = PCA(n_components=2)
+        data_embedded = pca.fit_transform(X)
+
     elif method == 't-SNE':
-        Z = perform_TSNE(X, perplexity = 10)
+        tsne = TSNE(n_components=2, verbose=1)
+        data_embedded = tsne.fit_transform(X)
     else:
         raise ValueError("Requested a method that is not supported")
-    points = pd.DataFrame(Z, columns=['posX', 'posY'])
-    points['cluster'] = y
-    # How to JSON serialize pandas dataframes and numpy arrays
-    return points.to_dict(orient='records'), list(target_names)
+    
+    df_embeddings = pd.DataFrame()
+    df_embeddings["cluster"] = predictions
+    df_embeddings['state'] = list(df['state'])
+    df_embeddings['year'] = list(df['year'])
+    df_embeddings["dimension1"] = data_embedded[:, 0]
+    df_embeddings["dimension2"] = data_embedded[:, 1]
+
+    return df_embeddings.to_dict(orient='records')
+
+
+
