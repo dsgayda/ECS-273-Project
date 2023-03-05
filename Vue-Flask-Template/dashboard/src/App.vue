f13e1264bf40f62270fc892095c61abfc08c83e5
David Gayda <david@Davids-MacBook-Pro-8.local>
2023-03-04 18:14:47 -0800

    updated layout

--- Vue-Flask-Template/dashboard/src/App.vue
+++ Vue-Flask-Template/dashboard/src/App.vue
@@ -19,45 +19,109 @@ export default {
 <!--This is using the grid component from Vuetify to do layout design-->
 <template>
   <v-container id="main-container" class="d-flex flex-column flex-nowrap" fluid>
+
     <v-row no-gutters>
-      <v-row no-gutters>
-        
-        <v-col cols="12">
-          Nested Row 1, Column 1
-          <PolicyScatterplot></PolicyScatterplot>
-        </v-col>
-         
-        <v-col cols="12" style="background-color: red;">
-          <!-- <v-row no-gutters style="background-color: grey;"> -->
-            <v-col cols="6" style="background-color: blue;">
-              
+
+      <!-- Left column -->
+      <v-col cols="6">
+
+        <!-- Top-left box -->
+        <v-row no-gutters style="height: 50%; padding-right: 5px; padding-bottom: 5px;">
+          <v-col cols="12" style="background-color: #FFD700; padding: 10px;">
+            Top-left box
+            <PolicyScatterplot></PolicyScatterplot>
+          </v-col>
+        </v-row>
+
+        <!-- Bottom-left box -->
+        <v-row no-gutters style="height: 50%; padding-right: 5px; padding-top: 5px; padding-bottom: 5px;">
+
+          <!-- Left column of bottom-left box -->
+          <v-col cols="8" style="height: 100%; padding: 0px;">
+
+            <!-- Top row of left column of bottom-left box -->
+            <v-row no-gutters style="height: 80%; padding-right: 5px; padding-bottom: 5px; padding-left: 5px;">
+              <v-col cols="12" style="background-color: #FFA07A; padding: 10px;">
+                Top row of left column of bottom-left box
               </v-col>
-              <v-col cols="6" style="background-color: green;">
-              
-            </v-col>
-          <!-- </v-row> -->
+            </v-row>
 
-        </v-col>
-      </v-row>
+            <!-- Bottom row of left column of bottom-left box -->
+            <v-row no-gutters style="height: 20%; padding-right: 5px; padding-left: 5px;">
+              <v-col cols="12" style="background-color: #87CEFA; height: 100%; overflow-y: auto; padding: 10px;">
+                Bottom row of left column of bottom-left box
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at sagittis justo. Aliquam blandit nibh
+                non dolor placerat viverra. Aenean euismod erat quis sem elementum pharetra. Donec molestie a nulla ut
+                bibendum. Proin sit amet aliquet eros, ut pretium lorem. Fusce id ipsum at ante elementum pulvinar. In hac
+                habitasse platea dictumst. Duis accumsan elit enim, vel pharetra magna finibus quis. Maecenas vel volutpat
+                libero. Donec ultrices lorem quis nisl gravida, sed scelerisque enim euismod.
+              </v-col>
+            </v-row>
 
-      <!-- <ExampleWithInteractions/> -->
-      <v-row no-gutters style="background-color: blue;">
-        <v-col cols="8">Nested Row 1, Column 9</v-col>
+          </v-col>
 
-        <v-col cols="4" style="background-color: purple;">Nested Row 1, Column 1</v-col>
-        <v-col cols="12" style="background-color: orange;">Nested Row 1, Column 2</v-col>
-      </v-row>
+          <!-- Right column of bottom-left box -->
+          <v-col cols="4" style="background-color: #7B68EE; height: 100%; padding: 10px;">
+            <v-row no-gutters>
+              <v-col cols="12">
+                Right column of bottom-left box
+              </v-col>
+            </v-row>
+          </v-col>
+
+        </v-row>
+
+      </v-col>
+
+      <!-- Right column -->
+      <v-col cols="6">
+
+        <!-- Top-right box -->
+        <v-row no-gutters style="height: 50%; padding-left: 5px; padding-bottom: 5px;">
+          <!-- Left column of top-right box -->
+          <v-col cols="8" style="background-color: #00FA9A; height: 100%; padding-left: 5px; padding: 10px;">
+            <v-row no-gutters>
+              <v-col cols="12">
+                Left column of top-right box
+              </v-col>
+            </v-row>
+          </v-col>
+
+          <!-- Right column of top-right box -->
+          <v-col cols="4" style="background-color: #DC143C; height: 100%; padding: 10px;">
+            <v-row no-gutters>
+              <v-col cols="12">
+                Right column of top-right box
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+
+        <!-- Bottom-right box -->
+        <v-row no-gutters style="height: 50%; padding-left: 5px; padding-top: 5px;">
+          <v-col cols="12" style="background-color: #FFA07A; padding: 10px;">
+            Bottom-right box
+          </v-col>
+        </v-row>
+
+      </v-col>
+
     </v-row>
-    <!-- <v-row no-gutters>
-        <v-col>
-          <Notes msg="This message is showcasing how to pass down the props"/>
-        </v-col>
-      </v-row> -->
   </v-container>
 </template>
 
-<style scoped>
-#main-container {
-  height: 100%;
-}
-</style>
+
+
+
+
+
+
+
+
+
+    <style scoped>
+      #main-container {
+      height: 100%;
+    }
+  </style>
