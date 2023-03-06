33aa4be01cfe04a87f29a0ccd7de48145ec62833
Namrata Kasaraneni <nammy.kasaraneni@gmail.com>
2023-03-05 16:47:20 -0800

    grouped bar chart backend done!!

--- Vue-Flask-Template/server/app.py
+++ Vue-Flask-Template/server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
-from controller import processExample, processPolicyScatterplot
+from controller import processPolicyScatterplot, processGroupedBarChart #, plotprocessExample
 
 app = Flask(__name__)
 CORS(app)
@@ -10,19 +10,19 @@ CORS(app)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/fetchExample", methods=["GET", "POST"])
-@cross_origin()
-def fetchExample():
-    if request.method == "GET": # handling GET request
-        points, cluster_names = processExample()
-        resp = jsonify(data=points, clusters=cluster_names)
-        return resp
-    else: # handling POST request, which is only effective when ExampleWithInteractions.vue is loaded
-        request_context = request.get_json() # JSON object
-        method = request_context['method']
-        points, cluster_names = processExample(method)
-        resp = jsonify(data=points, clusters=cluster_names)
-        return resp
+# @app.route("/fetchExample", methods=["GET", "POST"])
+# @cross_origin()
+# def fetchExample():
+#     if request.method == "GET": # handling GET request
+#         points, cluster_names = processExample()
+#         resp = jsonify(data=points, clusters=cluster_names)
+#         return resp
+#     else: # handling POST request, which is only effective when ExampleWithInteractions.vue is loaded
+#         request_context = request.get_json() # JSON object
+#         method = request_context['method']
+#         points, cluster_names = processExample(method)
+#         resp = jsonify(data=points, clusters=cluster_names)
+#         return resp
 
 
 @app.route("/fetchPolicyScatterplot", methods=["GET", "POST"])
@@ -30,6 +30,14 @@ def fetchExample():
 def fetchPolicyScatterplot():
     points, cluster_names = processPolicyScatterplot()
     resp = jsonify(data=points, clusters=cluster_names)
+    return resp
+
+@app.route("/fetchGroupedBarChart", methods=["GET", "POST"])
+@cross_origin()
+def fetchGroupedBarChart():
+    points, cluster_names = processPolicyScatterplot()
+    bars = processGroupedBarChart(points, cluster_names)
+    resp = jsonify(data=bars, clusters=cluster_names)
     return resp
 
 
--- Vue-Flask-Template/server/controller.py
+++ Vue-Flask-Template/server/controller.py
@@ -6,28 +6,28 @@ from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 
 from sklearn.datasets import load_wine
-from resources.hd_processing_template import perform_PCA, perform_TSNE
+# from resources.hd_processing_template import perform_PCA, perform_TSNE
 #from resources.network_process_template import contsruct_networkx
 #from resources.text_processing_template import preprocess
 #from resources.time_processing_template import prepare_time_template_data, apply_arima, apply_sarima
 
-def processExample(method: str = 'PCA') -> tuple[list[dict], list[int]]:
-    data: dict = load_wine()
-    X: np.ndarray = data.data
-    y: np.ndarray = data.target
-    #feat_names: np.ndarray = data.feature_names
-    target_names: np.ndarray = data.target_names
+# def processExample(method: str = 'PCA') -> tuple[list[dict], list[int]]:
+    # data: dict = load_wine()
+    # X: np.ndarray = data.data
+    # y: np.ndarray = data.target
+    # #feat_names: np.ndarray = data.feature_names
+    # target_names: np.ndarray = data.target_names
 
-    if method == 'PCA':
-        Z, _ = perform_PCA(X)
-    elif method == 't-SNE':
-        Z = perform_TSNE(X, perplexity = 10)
-    else:
-        raise ValueError("Requested a method that is not supported")
-    points = pd.DataFrame(Z, columns=['posX', 'posY'])
-    points['cluster'] = y
-    # How to JSON serialize pandas dataframes and numpy arrays
-    return points.to_dict(orient='records'), list(target_names)
+    # if method == 'PCA':
+    #     Z, _ = perform_PCA(X)
+    # elif method == 't-SNE':
+    #     Z = perform_TSNE(X, perplexity = 10)
+    # else:
+    #     raise ValueError("Requested a method that is not supported")
+    # points = pd.DataFrame(Z, columns=['posX', 'posY'])
+    # points['cluster'] = y
+    # # How to JSON serialize pandas dataframes and numpy arrays
+    # return points.to_dict(orient='records'), list(target_names)
 
 
 def preprocessGunViolenceData():
@@ -73,6 +73,36 @@ def preprocessGunViolenceData():
     df.to_pickle("../server/data/gunViolenceData.pickle")
 
 
+def preprocessGunViolenceMetadata():
+    gun_violence_data_filepath = '../server/data/gunViolenceData.pickle'
+    population_data_filepath = '../server/data/populationData.xlsx'
+
+    if not os.path.exists(os.getcwd() + gun_violence_data_filepath):
+        preprocessGunViolenceData()
+    
+    # create gun violence metadata
+    gun_violence_data = pd.read_pickle(gun_violence_data_filepath)
+    all_incidents = gun_violence_data[['year', 'state']].groupby(['state', 'year']).size()
+    all_incidents.name = 'all incidents'
+    gun_violence_metadata = gun_violence_data[['year', 'state', 'suicide', 'mass shooting', 'gang', 'non-suicide']].groupby(['state', 'year']).sum().astype(int)
+    gun_violence_metadata = gun_violence_metadata.join(all_incidents)
+
+    # load and preprocess population data
+    population_data = pd.read_excel(population_data_filepath, header=3, skiprows=[62, 63, 64, 65, 66, 67])
+    population_data.rename(columns={'July 1': 2020}, inplace=True)
+    population_data.dropna(inplace=True)
+    population_data['state'] = population_data['Unnamed: 0'].str.replace('[^\w\s]','')
+    population_data = pd.melt(population_data, id_vars=['state'], value_vars=[2014, 2015, 2016, 2017, 2018, 2019, 2020])
+    population_data.rename(columns={'variable': 'year', 'value':'population'}, inplace=True)
+
+    # get metadata per capita instead of total counts
+    gun_violence_metadata['population'] = list(gun_violence_metadata.merge(population_data, on=['year', 'state'], how='left')['population'])
+    gun_violence_metadata = gun_violence_metadata.div(gun_violence_metadata['population'], axis=0).drop(columns=['population'])
+
+    # store data
+    gun_violence_metadata.to_pickle('../server/data/gunViolenceMetadata.pickle') 
+
+
 def preprocessPolicyMetadata():
     policy_data_filepath = "../server/data/policyDatabase.xlsx"
     policy_codebook_filepath = "../server/data/policyCodebook.xlsx"
@@ -99,44 +129,16 @@ def preprocessPolicyMetadata():
     metadata_df = pd.DataFrame(metadata)
     metadata_df.sort_values(['year', 'state', 'category'], inplace=True)
     metadata_df = metadata_df[metadata_df.year > 2013]
-    metadata_df = metadata_df[metadata_df.year < 2021]
+    metadata_df = metadata_df[metadata_df.year < 2019]
 
-    metadata_df.to_pickle('../server/data/policyMetadata.pickle', index=False)
-
-def preprocessGunViolenceMetadata():
-    gun_violence_data_filepath = '../server/data/gunViolenceData.pickle'
-    population_data_filepath = '../server/data/populationData.xlsx'
-
-    if not os.path.exists(os.getcwd() + gun_violence_data_filepath):
-        preprocessGunViolenceData()
-    
-    # create gun violence metadata
-    gun_violence_data = pd.read_pickle(gun_violence_data_filepath)
-    all_incidents = gun_violence_data[['year', 'state']].groupby(['state', 'year']).size()
-    all_incidents.name = 'all incidents'
-    gun_violence_metadata = gun_violence_data[['year', 'state', 'suicide', 'mass shooting', 'gang', 'non-suicide']].groupby(['state', 'year']).sum().astype(int)
-    gun_violence_metadata = gun_violence_metadata.join(all_incidents)
-
-    # load and preprocess population data
-    population_data = pd.read_excel(population_data_filepath)
-    population_data.rename(columns={'July 1': 2020}, inplace=True)
-    population_data.dropna(inplace=True)
-    population_data['state'] = population_data['Unnamed: 0'].str.replace('[^\w\s]','')
-    population_data = pd.melt(population_data, id_vars=['state'], value_vars=[2014, 2015, 2016, 2017, 2018, 2019, 2020])
-    population_data.rename(columns={'variable': 'year', 'value':'population'}, inplace=True)
-
-    # get metadata per capita instead of total counts
-    gun_violence_metadata['population'] = list(gun_violence_metadata.merge(population_data, on=['year', 'state'], how='left')['population'])
-    gun_violence_metadata = gun_violence_metadata.div(gun_violence_metadata['population'], axis=0).drop(columns=['population'])
-
-    # store data
-    gun_violence_metadata.to_pickle('../server/data/gunViolenceMetadata.pickle')   
+    metadata_df.to_pickle('../server/data/policyMetadata.pickle', index=False)  
 
 
 def processPolicyScatterplot(num_clusters: int = 3, method: str = 'PCA'):
     # load data
     data_filepath = "../server/data/policyDatabase.xlsx"
     df = pd.read_excel(data_filepath)
+    df = df[(df.year > 2013) & (df.year < 2019)]
     X = df.drop(columns=['state', 'year', 'lawtotal'])
 
     # cluster data
@@ -154,7 +156,7 @@ def processPolicyScatterplot(num_clusters: int = 3, method: str = 'PCA'):
         raise ValueError("Requested a method that is not supported")
     
     df_embeddings = pd.DataFrame()
-    df_embeddings["cluster"] = predictions
+    df_embeddings['cluster'] = predictions
     df_embeddings['state'] = list(df['state'])
     df_embeddings['year'] = list(df['year'])
     df_embeddings["dimension1"] = data_embedded[:, 0]
@@ -167,8 +169,33 @@ def processPolicyScatterplot(num_clusters: int = 3, method: str = 'PCA'):
     return df_embeddings.to_dict(orient='records'), list(cluster_names)
 
 
-def processGroupedBarChart(cluster_labels: list):
-    if not os.path.exists(os.getcwd() +'../server/data/gunViolenceMetadata.pickle'):
+def processGroupedBarChart(policy_clusters: dict, cluster_names:list):
+    """
+    I'm expecting clusters to be the same data that is outputted 
+    by processPolicyScatterplot
+    NOTE: Might need to log scale the bar chart data because numbers are so small
+    """
+    gun_violence_metadata_filepath = '../server/data/gunViolenceMetadata.pickle'
+    if not os.path.exists(os.getcwd() + gun_violence_metadata_filepath):
         preprocessGunViolenceMetadata()
+
+    gun_violence_metadata = pd.read_pickle(gun_violence_metadata_filepath)
+    policy_clusters = pd.DataFrame(policy_clusters)
+    policy_clusters.set_index(['state', 'year'], inplace=True)
+    print(policy_clusters.index)
+    all_data = gun_violence_metadata.join(policy_clusters)
+
+    data = []
+    for cluster in range(len(cluster_names)):
+        cluster_data = all_data[all_data.cluster == cluster]
+        cluster_stats = {}
+        for stat in gun_violence_metadata.columns:
+            cluster_stats[stat] = cluster_data[stat].mean()
+        data.append(cluster_stats)
+    
+    return data
+
+
+
     
 
