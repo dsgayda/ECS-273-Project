e326826695b08a747becb3b27957cfb3d250e845
David Gayda <david@Davids-MacBook-Pro-8.local>
2023-03-09 18:15:13 -0800

    chart layout and color modifications

--- Vue-Flask-Template/dashboard/src/components/GroupedBarChart.vue
+++ Vue-Flask-Template/dashboard/src/components/GroupedBarChart.vue
@@ -19,10 +19,11 @@ export default {
         const { size } = storeToRefs(store);
         const { margin } = storeToRefs(store);
         const { clusters } = storeToRefs(store);
-
+        const { color }    = storeToRefs(store);
         return {
             // store, // Return store as the local state, but when you update the property value, the store is also updated.
-            resize,
+            // resize,
+            color,
             bars,
             size,
             margin,
@@ -44,20 +45,32 @@ export default {
             this.size = { width: target.clientWidth, height: target.clientHeight };
         },
         initChart() {
+            let gbc = this.$refs.groupedBarContainer as HTMLElement;
+            let svg = d3.select(gbc)
+                .append('svg')
+                .attr('id', 'grouped-bar-svg')
+                // .attr('width', '100%')
+                // .attr('height', '100%')
+                // .style('display', 'block');
+
+            const parentRect = gbc.getBoundingClientRect();
+            // svg.attr('viewBox', `${this.margin.left} ${this.margin.top} ${parentRect.width} ${parentRect.height }`);
+
+            svg.attr('viewBox', `${this.margin.left} ${this.margin.top} ${parentRect.width} ${parentRect.height }`);
+
+
+            // let chartContainer = svg;
+
             // append the svg object to the body of the page
-            var svg = d3.select("#grouped-bar-svg")
             // List of groups = gun violence stats
             var groups = this.bars.map(d => d.group)
             // List of clusters
             var subgroups = this.clusters
-            // Add X axis
+            
             const x = d3.scaleBand()
                 .domain(groups)
-                .range([this.margin.left, this.size.width - this.margin.right])
+                .range([0, parentRect.width - (this.margin.right + this.margin.left)])
                 .padding([0.2])
-            svg.append("g")
-                .attr("transform", `translate(0, ${this.size.height - this.margin.bottom})`)
-                .call(d3.axisBottom(x).tickSize(0));
             
             let y_max = 0
             for(let elem of this.bars){
@@ -69,43 +82,71 @@ export default {
                     }
                 }
             }
-            // Add Y axis
+
+            console.log('y max: ', y_max)
+            
             var y = d3.scaleLinear()
-                .domain([y_max, 0])
+                .domain([0, y_max])
                 // .domain([40, 0])
-                .range([ this.margin.bottom, this.size.height - this.margin.top ]);
-            svg.append("g")
-                .attr('transform', `translate(${this.margin.left}, ${0})`)
-                .call(d3.axisLeft(y)
-                .tickFormat(d3.format(".1e"))
-                );
+                .range([ parentRect.height - this.margin.bottom, this.margin.top]);
+            
+                
             // Another scale for subgroup position?
             var xSubgroup = d3.scaleBand()
                 .domain(subgroups)
                 .range([0, x.bandwidth()])
                 .padding([0.05])
             // color palette = one color per subgroup
-            var color = d3.scaleOrdinal()
-                .domain(this.clusters)
-                .range(d3.schemeSet1)
+            var color = this.color;
+            // d3.scaleOrdinal()
+            //     .domain(this.clusters)
+            //     .range(d3.schemeSet1)
             // Show the bars
-            svg.append("g")
-                .selectAll("g")
+            svg
+                .selectAll("rect")
                 // Enter in data = loop group per group
                 .data(this.bars)
-                .join("g")
-                .attr("transform", d => `translate(${x(d.group)}, ${this.margin.bottom - 80})`)
+                .join("g") 
+                .attr("transform", d => `translate(${x(d.group)}, ${0})`)
                 .selectAll("rect")
                 .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
                 .join("rect")
                 .attr("x", d => xSubgroup(d.key))
-                .attr("y", d => y(d.value))
+                .attr("y", d => {
+                    console.log('data: ', d.value)
+
+                    console.log('y_max: ', y(y_max))
+                    return y(d.value)
+                })
                 .attr("width", xSubgroup.bandwidth())
-                .attr("height", (d) => {return this.size.height - y(d.value)})
-                .attr("fill", d => color(d.key));
+                .attr("height", (d) => {return parentRect.height - y(d.value) - this.margin.bottom})
+                .attr("fill", d => {
+
+                    console.log('color key: ', parseInt(d.key.substring(d.key.length - 1, d.key.length)) - 1)
+
+                    console.log('color key: ', color(d.key))
+                    return color((parseInt(d.key.substring(d.key.length - 1, d.key.length)) - 1).toString())
+                })
+                .attr("transform", `translate(${this.margin.left + this.margin.right+ 20}, 0)`)
+                ;
+
+            // Add X axis
+            svg.append("g")
+                .attr("transform", `translate(${this.margin.left + this.margin.right+ 20}, ${parentRect.height - this.margin.bottom})`)
+                .call(d3.axisBottom(x).tickSize(0));
+                
+            // Add Y axis
+            svg.append("g")
+                .attr('transform', `translate(${this.margin.left + this.margin.right + 20}, 0)`)
+                .call(d3.axisLeft(y)
+                .tickFormat(d3.format(".1e"))
+                
+                );
+
+            
         },
         rerender() {
-            d3.selectAll('#grouped-bar-svg').selectAll('*').remove() // Clean all the elements in the chart
+            d3.selectAll('.chart-container').selectAll('*').remove() // Clean all the elements in the chart
             this.initChart()
         }
     },
@@ -138,9 +179,7 @@ export default {
 <!-- We use flex to arrange the layout-->
 <template>
     <div class="chart-container d-flex" ref="groupedBarContainer">
-        <svg id="grouped-bar-svg" width="100%" height="100%">
-            <!-- all the visual elements we create in initChart() will be inserted here in DOM-->
-        </svg>
+        
     </div>
 </template>
 
